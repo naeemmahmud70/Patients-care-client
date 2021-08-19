db6d2e28e889b9e7460fa88f70689b7f08742877
Naeem Mahmud Al-Amin <naeemmahmud370@gmail.com>
2021-08-19 17:55:34 +0600

    appointment page done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Route
 } from "react-router-dom";
 import Home from './components/Home/Home/Home';
+import Appointment from './components/Appointment/Appintment/Appointment';
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
       <Switch>
         <Route exact path="/">
           <Home></Home>
+        </Route>
+        <Route path="/appointment">
+          <Appointment></Appointment>
         </Route>
       </Switch>
     </Router>
---
+++ src/components/Appointment/AppointmentForm/AppointmentForm.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import Modal from 'react-modal';
+import { useForm } from "react-hook-form";
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
+Modal.setAppElement('#root')
+
+const AppointmentForm = ({ modalIsOpen, closeModal, appointmentOn, date }) => {
+    
+    const { register, handleSubmit, errors } = useForm();
+
+    const onSubmit = data => {
+        console.log(data)
+    }
+    return (
+        <div>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+
+                <h2 className="text-brand text-center">{appointmentOn}</h2>
+                <p className="text-secondary text-center "><small>On {date.toDateString()}</small></p>
+                <form onSubmit={handleSubmit(onSubmit)}>
+
+                    <div className="form-group">
+                        <input type="text" ref={register} name="name" placeholder="Your Name" className="form-control" />
+                        {errors.name && <span className="text-danger">This field is required</span>}
+
+                    </div>
+                    <div className="form-group">
+                        <input type="text" ref={register} name="phone" placeholder="Phone Number" className="form-control" />
+                        {errors.phone && <span className="text-danger">This field is required</span>}
+                    </div>
+                    <div className="form-group">
+                        <input type="text" ref={register} name="email" placeholder="Email" className="form-control" />
+                        {errors.email && <span className="text-danger">This field is required</span>}
+                    </div>
+                    <div className="form-group row">
+                        <div className="col-4">
+
+                            <select className="form-control" name="gender" ref={register} >
+                                <option disabled={true} value="Not set">Select Gender</option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                                <option value="Not set">Other</option>
+                            </select>
+                            {errors.gender && <span className="text-danger">This field is required</span>}
+
+                        </div>
+                        <div className="col-4">
+                            <input ref={register} className="form-control" name="age" placeholder="Your Age" type="number" />
+                            {errors.age && <span className="text-danger">This field is required</span>}
+                        </div>
+                        <div className="col-4">
+                            <input ref={register} className="form-control" name="weight" placeholder="Weight" type="number" />
+                            {errors.weight && <span className="text-danger">This field is required</span>}
+                        </div>
+                    </div>
+
+                    <div className="form-group text-right">
+                        <button type="submit" className="btn btn-brand">Send</button>
+                    </div>
+                </form>
+            </Modal>
+        </div>
+    );
+};
+
+export default AppointmentForm;
